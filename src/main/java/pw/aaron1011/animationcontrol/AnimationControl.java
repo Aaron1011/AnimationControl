936f5b8baf2c8b99c2508c593ca9617a7eb9082e
Aaron Hill <aa1ronham@gmail.com>
2018-07-31 14:26:47 -0400

    Initial commit

---
+++ src/main/java/pw/aaron1011/animationcontrol/AnimationControl.java
@@ -0,0 +1,19 @@
+package pw.aaron1011.animationcontrol;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(
+        modid = AnimationControl.MOD_ID,
+        name = AnimationControl.MOD_NAME,
+        version = AnimationControl.VERSION
+)
+public class AnimationControl {
+
+    public static final String MOD_ID = "animationcontrol";
+    public static final String MOD_NAME = "AnimationControl";
+    public static final String VERSION = "1.0.0-SNAPSHOT";
+
+    /** This is the instance of your mod as created by Forge. It will never be null. */
+    @Mod.Instance(MOD_ID)
+    public static AnimationControl INSTANCE;
+}
